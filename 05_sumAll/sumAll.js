2de619bdf1c4cb3d3fe5da94aa4be21c883b73ee
Elik <130469789+elik-dev@users.noreply.github.com>
2023-05-02 21:30:54 +0600

    Complete 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (
+        a < 0 
+        || b < 0
+        || typeof (a + b) !== 'number'
+        ) {
+        return "ERROR";
+    }
+    let minValue = Math.min(a, b);
+    let maxValue = Math.max(a, b);
+    let output = 0;
+    for (let i = minValue; i <= maxValue; i++) {
+        output += i;
+    }
+    return output;
 };
 
 // Do not edit below this line
